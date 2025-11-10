5c42bdd2f269d30c020707d6177336b8ed00961b
default <marco.cantusci@stu.ocsb.ca>
2025-11-10 11:07:26 +0000

    Finished A2

---
+++ A2.go
@@ -0,0 +1,28 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-10
+ * @fileoverview Calculate and display the side length of a cube with a volume of 1000 mm³.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// Stores volume as 1000
+	const VOLUME float64 = 1000
+
+	// formula/calculation of side length
+	var sideLength float64 = VOLUME / 100
+
+	// print volume
+	fmt.Println("Volume:" ,VOLUME, "mm³")
+
+	// print side length
+	fmt.Println("The side length of a cube that has a volume of" ,VOLUME, "mm³ is" ,sideLength, "mm")
+
+	fmt.Println("\nDone.")
+
+	}
---
+++ A2.ts
@@ -0,0 +1,20 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-10
+ * @fileoverview Calculate and display the side length of a cube with a volume of 1000 mm³.
+ */
+
+// stores volume as 1000
+const VOLUME: number = 1000;
+
+// formula/calculation of side length
+let sideLength: number = VOLUME / 100;
+
+// print volume
+console.log("Volume:" + VOLUME + "mm³");
+
+// print side length
+console.log("The side length of a cube that has a volume of " + VOLUME + "mm³ is" + sideLength + "mm");
+
+console.log("\nDone.");
